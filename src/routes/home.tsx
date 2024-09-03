34c7f3cbefb7c5ecce904b60d31eb42248272cce
M Daniel  Dantas <contatomarcosdgomes@gmail.com>
2024-09-03 20:56:51 -0300

    chore: Add user state to Home component

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,7 +1,14 @@
+// impotando o componente Search 
 import Search from "../components/Search";
+// importando o useState do react  para criar um estado local para o componente Home
+import { useState } from "react";
+
 
 // criando o componente Home
 const Home = () => {
+  // criando o estado local user e setUser
+const [user, setUser] = useState(null);
+
   return <div>
     <Search />
     </div>;
