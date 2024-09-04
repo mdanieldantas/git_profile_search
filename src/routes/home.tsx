663826f9b06b1b2ee09ac68d4b0d5c97dc35641b
M Daniel  Dantas <contatomarcosdgomes@gmail.com>
2024-09-03 21:14:45 -0300

    feat: Add UserProps type and update Home component
    The code changes include:
    - Importing the `UserProps` type from `../types/user.ts`
    - Updating the `user` state in the `Home` component to be of type `UserProps|null`
    
    This commit adds the `UserProps` type and updates the `Home` component to use the new type for the `user` state.

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,3 +1,6 @@
+
+// impotando o componente User e o UserProps da aplicação 
+import { UserProps } from "../types/user";
 // impotando o componente Search 
 import Search from "../components/Search";
 // importando o useState do react  para criar um estado local para o componente Home
@@ -6,8 +9,8 @@ import { useState } from "react";
 
 // criando o componente Home
 const Home = () => {
-  // criando o estado local user e setUser
-const [user, setUser] = useState(null);
+  // criando o estado local para o componente Home e o setUser para atualizar o estado local começa por null e vai receber um objeto do tipo UserProps
+const [user, setUser] = useState<UserProps|null>(null);
 
   return <div>
     <Search />
