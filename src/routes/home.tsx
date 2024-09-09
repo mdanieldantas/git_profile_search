8f6a0525b10c62859de2d10fdf3b2d5608a7b2fd
M Daniel  Dantas <contatomarcosdgomes@gmail.com>
2024-09-09 14:24:12 -0300

    feat: Adicionar componente Loader para exibir um spinner de carregamento
    - Adiciona o arquivo Loader.module.css com estilos para o componente Loader
    - Adiciona o arquivo Loader.tsx com a implementação do componente Loader
    - Importa o componente Loader no arquivo home.tsx e renderiza o spinner de carregamento enquanto os dados estão sendo carregados

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -4,11 +4,12 @@ import { UserProps } from "../types/user";
 import Search from "../components/Search";
 // Importando o useState do React para criar um estado local no componente Home
 import { useState } from "react";
-
+// Importando o componente User
 import User from "../components/User";
-
+// Importando o componente Error
 import Error from "../components/Error";
-
+// Importando o componente Loader
+import Loader from "../components/Loader";
 
 // Criando o componente Home
 const Home = () => {
@@ -16,10 +17,19 @@ const Home = () => {
   // O estado inicial é null e ele pode receber um objeto do tipo UserProps
   const [user, setUser] = useState<UserProps | null>(null);
 
+  // Criando um estado local chamado 'error' e uma função 'setError' para atualizar esse estado.
+  // O estado inicial é false, indicando que não há erro inicialmente
   const [error, setError] = useState(false);
+
+  // Criando um estado local chamado 'isLoading' e uma função 'setLoading' para atualizar esse estado.
+  // O estado inicial é false, indicando que não está carregando inicialmente
+  const [isLoading, setLoading] = useState(false);
 
   // Função assíncrona para carregar os dados do usuário a partir do GitHub
   const loadUser = async (userName: string) => {
+    // Define o estado de carregamento como true
+    setLoading(true);
+    // Resetando o estado de erro para false e o estado de usuário para null
     setError(false);
     setUser(null);
     // Fazendo uma requisição para a API do GitHub com o nome de usuário fornecido
@@ -28,7 +38,11 @@ const Home = () => {
     // Convertendo a resposta para JSON
     const data = await res.json();
 
-    if(res.status === 404) {
+    // Define o estado de carregamento como false
+    setLoading(false);
+
+    // Se o status da resposta for 404 (não encontrado), define o estado de erro como true e retorna
+    if (res.status === 404) {
       setError(true);
       return;
     }
@@ -38,11 +52,11 @@ const Home = () => {
 
     // Criando um objeto do tipo UserProps com os dados extraídos
     const userData: UserProps = {
-      avatar_url,
-      login,
-      location,
-      followers,
-      following
+      avatar_url, // URL do avatar do usuário
+      login, // Nome de usuário
+      location, // Localização do usuário
+      followers, // Número de seguidores
+      following, // Número de pessoas que o usuário está seguindo
     };
 
     // Atualizando o estado 'user' com os dados do usuário
@@ -50,15 +64,22 @@ const Home = () => {
   };
 
   // Renderizando o componente Search e passando a função loadUser como prop
-  // Se o estado 'user' não for null, renderiza o login do usuário
+  // Renderiza o componente Loader enquanto os dados estão sendo carregados
+  // Se o estado 'user' não for null, renderiza o componente User com os dados do usuário
+  // Se o estado 'error' for true, renderiza o componente Error
   return (
     <div>
+      {/* Renderiza o componente Search e passa a função loadUser como prop */}
       <Search loadUser={loadUser} />
+      {/* Renderiza o componente Loader enquanto os dados estão sendo carregados */}
+      {isLoading && <Loader />}
+      {/* Se o estado 'user' estiver definido, renderiza o componente User com os dados do usuário */}
       {user && <User {...user} />}
+      {/* Se o estado 'error' for true, renderiza o componente Error */}
       {error && <Error />}
     </div>
   );
 };
 
-// Exportando o componente Home
+// Exportando o componente Home para ser usado em outros lugares da aplicação
 export default Home;
